178cbb106cac1fc215857438da667560f9d0d8d3
Benedikt Bauer <benedikt@xenzilla.de>
2018-10-18 21:00:05 +0200

    Adjust test to cover DE (success) and DK (fail)

--- test/test.py
+++ test/test.py
@@ -13,14 +13,14 @@ class TestTaxProvider(unittest.TestCase):
         for i in range(10):
             self.assertTrue(isinstance(fake.tax_id(), str))
 
-    def test_bg_BG(self):
-        fake = Faker('bg_BG')
+    def test_de_DE(self):
+        fake = Faker('de_DE')
         fake.add_provider(Provider)
         for i in range(10):
             tax_id = fake.tax_id()
             self.assertTrue(isinstance(tax_id, str))
-            self.assertGreaterEqual(11, len(tax_id))
-            self.assertLessEqual(12, len(tax_id))
+            self.assertEqual(11, len(tax_id))
+            self.assertEqual('DE', tax_id[:2])
 
     def test_dk_DK(self):
         fake = Faker('dk_DK')
@@ -28,4 +28,5 @@ class TestTaxProvider(unittest.TestCase):
         for i in range(10):
             tax_id = fake.tax_id()
             self.assertTrue(isinstance(tax_id, str))
+            self.assertEqual('DK', tax_id[:2])
             self.assertEqual(10, len(tax_id))
